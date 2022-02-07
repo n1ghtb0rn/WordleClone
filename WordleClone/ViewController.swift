9b6410b11705e81c875a49d239c4e0f3ce200724
Daniel Falkedal <daniel.falkedal@gmail.com>
2022-02-07 16:32:55 +0100

    update UI

--- WordleClone/ViewController.swift
+++ WordleClone/ViewController.swift
@@ -53,17 +53,23 @@ class ViewController: UIViewController {
     
     var inputs = [[UITextField]]()
     
+    let yellowColor: UIColor = #colorLiteral(red: 0.7079477906, green: 0.6244826913, blue: 0.2295816839, alpha: 1)
+    let greenColor: UIColor = #colorLiteral(red: 0.3271112442, green: 0.5547715425, blue: 0.3070441782, alpha: 1)
+    let grayColor: UIColor = .darkGray
+    
     var currentRowIndex = 0
     
     @IBOutlet weak var bottomLabel: UILabel!
     
-    let words = ["ABCDE", "EDCBA", "FFFFF"]
+    var words = [String]()
     
     var targetWord: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.loadWordlist()
         
         targetWord = words.randomElement()!
         
@@ -136,7 +142,7 @@ class ViewController: UIViewController {
         let currentRow = inputs[currentRowIndex]
         
         for input in currentRow {
-            answerString.append(input.text!)
+            answerString.append(input.text!.lowercased())
         }
         
         let answer = Array(answerString)
@@ -161,43 +167,69 @@ class ViewController: UIViewController {
         
         var correctLetters: Int = 0
         
+        var colors = [UIColor]()
         for i in 0...answer.count-1 {
-            currentRow[i].backgroundColor = .darkGray
+            colors.append(grayColor)
+        }
+        
+        for i in 0...answer.count-1 {
+            //currentRow[i].backgroundColor = grayColor
             for j in 0...target.count-1 {
                 if answer[i] == target[j] {
                     
                     if i != j {
-                        currentRow[i].backgroundColor = .yellow
+                        if colors[i] != greenColor {
+                            colors[i] = yellowColor
+                        }
                     }
                     else {
-                        currentRow[i].backgroundColor = .green
+                        colors[i] = greenColor
                         correctLetters += 1
                     }
                 }
             }
         }
         
-        for input in currentRow {
-            input.isEnabled = false
+        var delay: Double = 0.0
+        let delayIncrease: Double = 0.2
+        
+        for colorIndex in 0...currentRow.count-1 {
+            delay += delayIncrease
+            
+            DispatchQueue.main.asyncAfter (deadline: .now() + delay) {
+                
+                currentRow[colorIndex].backgroundColor = colors[colorIndex]
+                
+            }
         }
         
-        if correctLetters == 5 {
-            print("Correct letters: \(correctLetters)")
-            return
+        DispatchQueue.main.asyncAfter (deadline: .now() + (delayIncrease * 6) ) {
+            
+            for input in currentRow {
+                input.isEnabled = false
+            }
+            
+            if correctLetters == 5 {
+                print("Correct letters: \(correctLetters)")
+                return
+            }
+            
+            self.currentRowIndex += 1
+            
+            if self.currentRowIndex >= self.inputs.count {
+                print("Current row index: \(self.currentRowIndex)")
+                return
+            }
+            
+            let nextRow = self.inputs[self.currentRowIndex]
+            for input in nextRow {
+                input.isEnabled = true
+                input.alpha = 1.0
+            }
+            
         }
         
-        currentRowIndex += 1
         
-        if currentRowIndex >= inputs.count {
-            print("Current row index: \(currentRowIndex)")
-            return
-        }
-        
-        let nextRow = inputs[currentRowIndex]
-        for input in nextRow {
-            input.isEnabled = true
-            input.alpha = 1.0
-        }
         
     }
     
@@ -242,6 +274,22 @@ class ViewController: UIViewController {
             }
         }
         
+    }
+    
+    private func loadWordlist() {
+    
+        do {
+            // This solution assumes  you've got the file in your bundle
+            if let path = Bundle.main.path(forResource: "wordlist", ofType: "txt"){
+                let data = try String(contentsOfFile:path, encoding: String.Encoding.utf8)
+                words = data.components(separatedBy: "\n")
+            }
+        } catch let err as NSError {
+            // do something with Error
+            print(err)
+        }
+        
+        print("List loaded!")
     }
     
 }
